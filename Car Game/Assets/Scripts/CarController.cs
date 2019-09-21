e0b5cb492155c245810e063484d0956bfbef1128
benp <benjampeterson@gmail.com>
2019-09-21 01:08:30 -0700

    added particles and worked on car balance

--- Car Game/Assets/Scripts/CarController.cs
+++ Car Game/Assets/Scripts/CarController.cs
@@ -33,6 +33,7 @@ public class CarController : MonoBehaviour
 
 	public CarParameters CarParams;
 
+    public ParticleSystem dirtSystem;
 	public Rigidbody body;
 
 	float wheelY;
@@ -45,7 +46,7 @@ public class CarController : MonoBehaviour
 		wheelY = FrontLeft.Parent.localPosition.y;
 	}
 
-	bool ProcessWheel(Wheel w, float angle, bool drive)
+	bool CheckWheel(Wheel w, float angle, bool drive)
 	{
 		Transform t = w.Parent;
 		Vector3 wheelP = t.localPosition;
@@ -57,7 +58,7 @@ public class CarController : MonoBehaviour
 		if (drive)
 		{
 			t.transform.localRotation = Quaternion.identity;
-			t.Rotate(Vector3.up, angle);
+            t.Rotate(Vector3.up, angle);
 			w.SpinDistance += Input.GetAxis("Vertical") * HorsePower * Time.deltaTime;
 		}
 		w.SpinDistance = Mathf.Clamp(w.SpinDistance, -TopSpinSpeed, TopSpinSpeed);
@@ -85,6 +86,14 @@ public class CarController : MonoBehaviour
 			Vector3 speedAgainstTread = Vector3.Project(pointVelocity, w.Parent.forward);
 			w.counterTread = speedAgainstTread;
 
+            float counterSpeed = speedAgainstTread.magnitude;
+            if (counterSpeed > 7.5f)
+            {
+                dirtSystem.transform.position = hit.point;
+                dirtSystem.transform.forward = hit.normal;
+                dirtSystem.Emit(4);
+            }
+
 			//FORWARD
 			Vector3 speedWithTread = Vector3.Project(pointVelocity, w.Parent.right);
 			float currentWheelSpeed = (speedWithTread.magnitude * Vector3.Dot(speedWithTread, w.Parent.right));
@@ -97,21 +106,12 @@ public class CarController : MonoBehaviour
 				speedAgainstTread * InertiaAbsorb + //counter skid momentum
 				speedAgainstTread.magnitude * t.right * InertiaReroute + //re-add skid momentum lost to forward direction
 				Mathf.Clamp(w.SpinDistance - currentWheelSpeed, -MaxTorque, MaxTorque) * TireGripStrength * w.Parent.right;
-
-            //if (!Input.GetKey(KeyCode.Space))
-            w.SpinDistance = w.SpinDistance + TimeScaledMultiply((w.SpinDistance - currentWheelSpeed), .25f);
-
-			body.AddForceAtPosition(w.lastForceApplied * Time.deltaTime * 60f, t.position);
-			body.angularVelocity = body.angularVelocity.normalized * TimeScaledMultiply(body.angularVelocity.magnitude,
-                1f - Mathf.Clamp01(speedAgainstTread.magnitude / SpinControl));
+            
+            w.SpinDistance = Mathf.Lerp(w.SpinDistance, currentWheelSpeed, .5f);
 
 			//Put wheel on the ground
 			t.position = Vector3.Lerp(oldPosition, t.position - transform.up * (hit.distance - WheelRadius * 2f), .2f);
-			
-			//DebugText.text += w.Parent.name + ": " + 
-			//	StringUtility.Substr(w.IdealWheelSpeed.ToString(), 0, 4)
-			//	+ " vs " + StringUtility.Substr(currentWheelSpeed.ToString(), 0, 4) 
-			//	+ "\n";
+            w.OnGround = true;
 
 			return true;
 		} else //if wheel not on ground
@@ -119,20 +119,53 @@ public class CarController : MonoBehaviour
 			wheelP.y = wheelY + Mathf.Min(0,Vector3.Project(-transform.up * SuspensionDistance, Vector3.up).magnitude * ((transform.up.y > 0f) ? -1f : 1f));
 			t.localPosition = Vector3.Lerp(oldLocalPosition, wheelP, .2f);
 			w.SuspensionActivation = 0f;
-			return false;
+            w.lastForceApplied = Vector3.zero;
+            w.OnGround = false;
+            return false;
 		}
 	}
+
+    void ApplyWheelForce(Wheel w)
+    {
+        if (w.OnGround)
+        {
+            body.AddForceAtPosition(w.lastForceApplied * Time.deltaTime * 60f, w.Parent.position);
+            body.angularVelocity = body.angularVelocity.normalized * TimeScaledMultiply(body.angularVelocity.magnitude,
+                1f - Mathf.Clamp01(w.counterTread.magnitude / SpinControl));
+
+            if (w.Drive)
+            {
+                Vector3 forwardVector = Vector3.Project(body.velocity, transform.right);
+                float forwardVelocity = forwardVector.magnitude * (((forwardVector - transform.right).magnitude < (forwardVector + transform.right).magnitude) ? -1f : 1f);
+
+                //Steering rotation of the wheel as a whole
+                float rotAmount = -forwardVelocity * wheelAngle * Time.deltaTime * .3f;
+
+                if (Mathf.Abs(wheelAngle) < 1f && Input.GetAxis("Horizontal") == 0f)
+                    wheelAngle = 0f;
+                else if (wheelAngle > 0f)
+                    wheelAngle = Mathf.Max(0f, wheelAngle - Mathf.Abs(rotAmount));
+                else if (wheelAngle < 0f)
+                    wheelAngle = Mathf.Min(0f, wheelAngle + Mathf.Abs(rotAmount));
+            }
+        }
+    }
 
 	public void Update()
 	{
 		DebugText.text = "";
-		bool fl = ProcessWheel(FrontLeft, wheelAngle, FrontLeft.Drive);
-		bool fr = ProcessWheel(FrontRight, wheelAngle, FrontRight.Drive);
-		bool bl = ProcessWheel(BackLeft, -wheelAngle, BackLeft.Drive);
-		bool br = ProcessWheel(BackRight, -wheelAngle, BackRight.Drive);
+		bool fl = CheckWheel(FrontLeft, wheelAngle, FrontLeft.Drive);
+		bool fr = CheckWheel(FrontRight, wheelAngle, FrontRight.Drive);
+		bool bl = CheckWheel(BackLeft, -wheelAngle, BackLeft.Drive);
+		bool br = CheckWheel(BackRight, -wheelAngle, BackRight.Drive);
 
-		//Air Tricks
-		if (!(fl || fr || bl || br))
+        ApplyWheelForce(FrontLeft);
+        ApplyWheelForce(FrontRight);
+        ApplyWheelForce(BackLeft);
+        ApplyWheelForce(BackRight);
+
+        //Air Tricks
+        if (!(fl || fr || bl || br))
 		{
 			if (Input.GetButton("Brake"))
 			{
@@ -144,19 +177,6 @@ public class CarController : MonoBehaviour
 		}
 		
 		wheelAngle = Mathf.Clamp(wheelAngle + TurnSpeed * Time.deltaTime * Input.GetAxis("Horizontal"), -MaxTurnAngle, MaxTurnAngle);
-		
-		Vector3 forwardVector = Vector3.Project(body.velocity, transform.right);
-		float forwardVelocity = forwardVector.magnitude * (((forwardVector - transform.right).magnitude < (forwardVector + transform.right).magnitude) ? -1f : 1f);
-
-		//Steering rotation of the wheel as a whole
-		float rotAmount = -forwardVelocity * wheelAngle * Time.deltaTime;
-
-		if (Mathf.Abs(wheelAngle) < 1f && Input.GetAxis("Horizontal") == 0f)
-			wheelAngle = 0f;
-		else if (wheelAngle > 0f)
-			wheelAngle = Mathf.Max(0f, wheelAngle - Mathf.Abs(rotAmount));
-		else if (wheelAngle < 0f)
-			wheelAngle = Mathf.Min(0f, wheelAngle + Mathf.Abs(rotAmount));
 
 		//Reset the car
 		if (Input.GetButtonDown("Cancel"))
@@ -167,7 +187,7 @@ public class CarController : MonoBehaviour
 			body.angularVelocity *= 0f;
 		}
 
-		DebugText.text = StringUtility.Substr((body.velocity.magnitude * 2.2369356).ToString(), 0, 3) + "mph";
+		DebugText.text = Mathf.Round(body.velocity.magnitude * 2.2369356f) + "mph";
 	}
 
     float TimeScaledMultiply(float f, float m)
@@ -210,10 +230,11 @@ public class Wheel
 {
 	public Transform Parent;
 	public Transform Geo;
-	public float SpinDistance;
-	public float SuspensionActivation;
-	public float VertSpeed;
-	public Vector3 lastForceApplied;
-	public Vector3 counterTread;
 	public bool Drive;
+    public float SpinDistance;
+    public float SuspensionActivation;
+    public float VertSpeed;
+    public Vector3 lastForceApplied;
+    public bool OnGround;
+    public Vector3 counterTread;
 }
